130b4ff3a7e262ed654965222f36222e82bd6c61
gfjgabriel <gfjgabriel@gmail.com>
2024-02-26 23:31:17 -0300

    Create Course subscription

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -1,7 +1,12 @@
 package com.ead.authuser.controllers;
 
-import com.ead.authuser.clients.UserClient;
+import com.ead.authuser.clients.CourseClient;
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.UserCourseDto;
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserCourseService;
+import com.ead.authuser.services.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -13,8 +18,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.Optional;
 import java.util.UUID;
 
 @Log4j2
@@ -23,14 +32,34 @@ import java.util.UUID;
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserCourseController {
 
-    private final UserClient userClient;
+    private final CourseClient courseClient;
+    private final UserService userService;
+    private final UserCourseService userCourseService;
 
     @GetMapping("/users/{userId}/courses")
     public ResponseEntity<Page<CourseDto>> getAllCoursesByUser(
             @PageableDefault(sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
             @PathVariable UUID userId
     ) {
-        Page<CourseDto> page = userClient.getAllCoursesByUser(userId, pageable);
+        Page<CourseDto> page = courseClient.getAllCoursesByUser(userId, pageable);
         return ResponseEntity.status(HttpStatus.OK).body(page);
+    }
+
+    @PostMapping("/users/{userId}/courses/subscription")
+    public ResponseEntity<Object> saveSubscriptionUserInCourse(
+            @PathVariable UUID userId,
+            @RequestBody @Valid UserCourseDto userCourseDto
+    ) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (userModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        UserModel userModel = userModelOptional.get();
+        if (userCourseService.existsByUserAndCourseId(userModel, userCourseDto.getCourseId())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Subscription already exists!");
+        }
+        UserCourseModel userCourseModel = userModel.convertToUserCourseModel(userCourseDto.getCourseId());
+        userCourseModel = userCourseService.save(userCourseModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCourseModel);
     }
 }
--- src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -1,14 +1,28 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.services.UserCourseService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 public class UserCourseServiceImpl implements UserCourseService {
 
-    private final UserCourseRepository userCourseRepository;
+    private final UserCourseRepository repository;
+
+    @Override
+    public boolean existsByUserAndCourseId(UserModel userModel, UUID courseId) {
+        return repository.existsByUserAndCourseId(userModel, courseId);
+    }
+
+    @Override
+    public UserCourseModel save(UserCourseModel userCourseModel) {
+        return repository.save(userCourseModel);
+    }
 }
