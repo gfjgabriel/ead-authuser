ccdfb4b488a41c01d02d33d249ec2a2d06c0563f
Gabriel Fortunato <gfjgabriel@gmail.com>
2025-06-17 23:27:47 -0300

    feature: add jwt authentication flow

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -1,5 +1,8 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.configs.security.JwtProvider;
+import com.ead.authuser.dtos.JwtDto;
+import com.ead.authuser.dtos.LoginDto;
 import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserStatus;
@@ -9,21 +12,20 @@ import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
-import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -31,32 +33,39 @@ import java.time.ZoneId;
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
-@AllArgsConstructor
 public class AuthenticationController {
 
-    private final UserService userService;
-    private final RoleService roleService;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(
-            @RequestBody @Validated({UserDto.UserView.RegistrationPost.class}) @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto
-    ) {
-        log.debug("POST registerUser userDto received {}", userDto);
-        if (userService.existsByUsername(userDto.getUsername())) {
-            log.warn("Username {} is already taken", userDto.getUsername());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is already taken");
+    public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
+                                                   @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto){
+        log.debug("POST registerUser userDto received {} ", userDto.toString());
+        if(userService.existsByUsername(userDto.getUsername())){
+            log.warn("Username {} is Already Taken ", userDto.getUsername());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
         }
-        if (userService.existsByEmail(userDto.getEmail())) {
-            log.warn("Email {} is already taken", userDto.getEmail());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is already taken");
+        if(userService.existsByEmail(userDto.getEmail())){
+            log.warn("Email {} is Already Taken ", userDto.getEmail());
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!");
         }
         RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_STUDENT)
-                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-
+                .orElseThrow(() -> new RuntimeException("Error: Role is Not Found."));
         userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
-
-        UserModel userModel = new UserModel();
+        var userModel = new UserModel();
         BeanUtils.copyProperties(userDto, userModel);
         userModel.setUserStatus(UserStatus.ACTIVE);
         userModel.setUserType(UserType.STUDENT);
@@ -64,8 +73,28 @@ public class AuthenticationController {
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
         userModel.getRoles().add(roleModel);
         userService.saveUser(userModel);
-        log.debug("POST registerUser userDto saved {}", userModel.getUserId());
-        log.info("User saved successfully userId {}", userModel.getUserId());
-        return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+        log.debug("POST registerUser userId saved {} ", userModel.getUserId());
+        log.info("User saved successfully userId {} ", userModel.getUserId());
+        return  ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtDto> authenticateUser(@Valid @RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtProvider.generateJwt(authentication);
+        return ResponseEntity.ok(new JwtDto(jwt));
+    }
+
+    @GetMapping("/")
+    public String index(){
+        log.trace("TRACE");
+        log.debug("DEBUG");
+        log.info("INFO");
+        log.warn("WARN");
+        log.error("ERROR");
+        return "Logging Spring Boot...";
     }
 }
